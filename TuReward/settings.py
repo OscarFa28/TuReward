d13c64f466b0623314658151e0d46def1b4d05da
Oscar Fabrizio de Alba Gtz <132043883+OscarFa28@users.noreply.github.com>
2025-01-11 15:29:13 +0100

    mysql replaced with sqllite to facilitate de production

--- TuReward/settings.py
+++ TuReward/settings.py
@@ -79,6 +79,8 @@ WSGI_APPLICATION = 'TuReward.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
+"""
 DATABASES  = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -92,6 +94,15 @@ DATABASES  = {
         },
     }
 }
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  
+    }
+}
+
 
 
 
